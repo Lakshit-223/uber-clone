2f21b0f1a52089e870c66d14250f04ab9e77e2c5
lakshit-223 <sharmaprince9001@gmail.com>
2024-12-07 13:59:30 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv')
+dotenv.config();
+// import express from 'express';
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
+module.exports = app;
