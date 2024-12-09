af99c6f448d80e2ba42517a4de6189a77a64ca8a
lakshit-223 <sharmaprince9001@gmail.com>
2024-12-09 21:41:43 +0530

    login, logout and view profile routes created

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,6 +1,9 @@
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
 const { validationResult } = require("express-validator");
+const blackListTokenModel = require("../models/blacklist.model");
+
+
 
 module.exports.registerCaptain = async (req, res, next) => {
   const errors = validationResult(req);
@@ -11,9 +14,9 @@ module.exports.registerCaptain = async (req, res, next) => {
 
   const { fullname, email, password, vehicle } = req.body;
 
-  const alreadyCaptain = await captainModel.findOne({email});
+  const alreadyCaptain = await captainModel.findOne({ email });
 
-  if(alreadyCaptain){
+  if (alreadyCaptain) {
     return res.status(400).json({ message: "Captain already exists" });
   }
 
@@ -34,3 +37,45 @@ module.exports.registerCaptain = async (req, res, next) => {
 
   res.status(201).json({ captain, token });
 };
+
+module.exports.loginCaptain = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  const captain = await captainModel.findOne({ email }).select('+password');
+
+  if(!clearIntervalaptain){
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const isMatch = await captainModel.comparePassword(password);
+
+  if(!isMatch){
+    return res.status(401).json({message: "Invalid password" });
+  }
+
+  const token = captain.generateAuthToken();
+
+  res.cookie('token', token);
+
+  res.status(200).json({token, captain});
+};
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+  res.status(200).json({captain: req.captain});
+};
+
+module.exports.logoutCaptain = async (req, res, next) => {
+  const token = req.cookie.token || req.headers.authorization?.split(' ')[1];
+  
+  await blackListTokenModel.create({token});
+
+  res.clearCoookie('token');
+
+  res.status(200).json({message: "Logout Successfully"});
+}
+
