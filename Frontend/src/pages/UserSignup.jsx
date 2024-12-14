565ccd0a1f7f5eba824df14fc64d33dc58d1b264
lakshit-223 <sharmaprince9001@gmail.com>
2024-12-14 16:51:44 +0530

    user signup part done

--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserDataContext } from "../context/UserContext";
 
 const UserSignup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-
   const [userData, setUserData] = useState({});
 
-  const submitHandler = (e) => {
+  const navigate = useNavigate();
+
+  const { user, setUser } = useContext(UserDataContext);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setUserData({
-      fullName: {
-        firstName: firstName,
-        lastName: lastName,
+    const newUser = {
+      fullname: {
+        firstname: firstName,
+        lastname: lastName,
       },
       email: email,
       password: password,
-    });
-    console.log(userData);
+    };
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/users/register`,
+      newUser
+    );
+
+    if (response.status === 201) {
+      const data = response.data;
+      setUser(data.user);
+      localStorage.setItem("token", data.token);
+      navigate("/home");
+    }
 
     setEmail("");
     setFirstName("");
@@ -32,79 +48,82 @@ const UserSignup = () => {
         <div>
           <img
             className="w-16 mb-10"
-            src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s"
+            alt=""
           />
+
           <form
             onSubmit={(e) => {
               submitHandler(e);
             }}
           >
-            <h3 className="text-base mb-2 font-medium">What's your name?</h3>
-            <div className="flex gap-4 mb-6">
+            <h3 className="text-lg w-1/2  font-medium mb-2">
+              What's your name
+            </h3>
+            <div className="flex gap-4 mb-7">
               <input
                 required
-                className="bg-[#eeeeee] rounded px-4 py2 border w-1/2 text-base placeholder:text-sm"
+                className="bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border  text-lg placeholder:text-base"
                 type="text"
-                placeholder="first name"
+                placeholder="First name"
                 value={firstName}
                 onChange={(e) => {
                   setFirstName(e.target.value);
                 }}
               />
               <input
-                className="bg-[#eeeeee] rounded px-4 py2 border w-1/2 text-base placeholder:text-sm"
+                required
+                className="bg-[#eeeeee] w-1/2  rounded-lg px-4 py-2 border  text-lg placeholder:text-base"
                 type="text"
-                placeholder="last name"
+                placeholder="Last name"
                 value={lastName}
                 onChange={(e) => {
                   setLastName(e.target.value);
                 }}
               />
             </div>
-            <h3 className="text-base mb-2 font-medium">What's your email?</h3>
+
+            <h3 className="text-lg font-medium mb-2">What's your email</h3>
             <input
               required
-              className="bg-[#eeeeee] mb-6 rounded px-4 py2 border w-full text-lg placeholder:text-sm"
-              type="email"
-              placeholder="email@example.com"
               value={email}
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
+              className="bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
+              type="email"
+              placeholder="email@example.com"
             />
-            <h3 className="text-base mb-2 font-medium">Enter Password</h3>
+
+            <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+
             <input
-              required
-              className="bg-[#eeeeee] mb-6 rounded px-4 py2 border w-full text-lg placeholder:text-sm"
-              type="password"
-              placeholder="*********"
+              className="bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base"
               value={password}
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
+              required
+              type="password"
+              placeholder="password"
             />
-            <button className="bg-[#111] text-white font-semibold mb-7 rounded px-4 py2 w-full text-lg placeholder:text-base">
-              Signup
+
+            <button className="bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base">
+              Create account
             </button>
-            <p className="text-center ">
-              Already have an account?{" "}
-              <Link to="/login" className="text-blue-600 ">
-                Login here
-              </Link>
-            </p>
           </form>
+          <p className="text-center">
+            Already have a account?{" "}
+            <Link to="/login" className="text-blue-600">
+              Login here
+            </Link>
+          </p>
         </div>
         <div>
-          <Link
-            to="/captain-signup"
-            className="bg-[#10b444] flex items-center justify-center text-white font-semibold mb-5 rounded px-4 py2 w-full text-lg placeholder:text-base"
-          >
-            Signup as Captain
-          </Link>
-          <p className="text-[#808080] text-[8px] leading-tight">
-            By proceeding, you consent to get calls, WhatsApp or SMS messages,
-            including by automated means, from Uber and its affiliates to email
-            provided.
+          <p className="text-[10px] leading-tight">
+            This site is protected by reCAPTCHA and the{" "}
+            <span className="underline">Google Privacy Policy</span> and{" "}
+            <span className="underline">Terms of Service apply</span>.
           </p>
         </div>
       </div>
